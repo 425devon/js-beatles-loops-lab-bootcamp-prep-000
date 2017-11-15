e73edf17a571c564c3d3a582e2e42860ae895a06
Devon Langendoerfer <425devon@gmail.com>
2017-11-15 05:50:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function theBeatlesPlay(person,instrument){
   }
   return member;
 }
+function johnLennonFacts(fact){
+  var exc = "!!!";
+  while(var i = 0; i<fact.length){
+    fact[i] = fact[i].concat(exc);
+  }
+  return  johnLennonFacts;
+}
