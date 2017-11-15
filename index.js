2abd59491883bac5611083ceadaf4517325ce1c9
Devon Langendoerfer <425devon@gmail.com>
2017-11-15 05:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function theBeatlesPlay(person,instrument){
   var member = [];
-  
+
   for(var i = 0; i < person.length; i++){
     member[i] = (person[i] + " plays " + instrument[i]);
   }
   return member;
 }
-
